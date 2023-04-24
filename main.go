7b0559c5ecd1a35b6fa0a7b324f8b9b8a80a04ac
Dima <vseravno123@yandex.ru>
2023-04-24 22:21:55 +0300

    Проба

--- main.go
+++ main.go
@@ -1,32 +1,7 @@
 package main
 
-type man struct {
-	name string
-	age  int
-}
-
-type woman struct {
-	name string
-	age  int
-}
-
-type skillz interface {
-	work()
-	rest()
-}
-
-func (m man) work(s string) {
-	return
-}
-
-func (w woman) rest(r string) {
-	return
-}
+import
 
 func main() {
-	dima := man{name: "Дима", age: 31}
-	lena := woman{name: "Лена", age: 30}
-	dima.work()
-	lena.rest()
-
+	test.test()
 }
